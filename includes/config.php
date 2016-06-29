49f433e9f779a5a198759bff5ab738f96829ec48
aemii <freelancerahmad@gmail.com>
2016-06-30 01:56:55 +0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "root";
-$mysql_database = "saloon";
+$mysql_hostname = "https://threesixzero-spasaloon.rhcloud.com/phpmyadmin/";
+$mysql_user = "adminsCZL1Nq";
+$mysql_password = "kqQyuytgeVGZ";
+$mysql_database = "threesixzero";
 
 $bd = mysql_connect($mysql_hostname,$mysql_user, $mysql_password) or
 die ("Something went wrong while connecting");
